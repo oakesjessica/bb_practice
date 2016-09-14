04d326d78b66b6819f11ecaf4a29167383b75747
Jessica Oakes <jessica.yoonah@gmail.com>
2016-09-13 23:44:57 -0500

    Move html to template w/ underscore

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 var Todo = Backbone.Model.extend({
   defaults: {
-    task : 'task title',
+    tasks : 'task title',
     status : false
   }
 });
@@ -8,12 +8,13 @@ var Todo = Backbone.Model.extend({
 var TodoView = Backbone.View.extend({
   tagName: 'li',
   className: 'todo-item',
+  todo_template: _.template('<%= tasks %>'),
   //  Constructor that is automatically called when view is initialized
   initialize: function() {
     this.render();
   },
   //  Method that renders output for model data associated with the view
   render: function() {
-    this.$el.html(this.model.get('task'));
+    this.$el.html(this.todo_template(this.model.toJSON()));
   }
 });
